6093530725b3656d09c1d79b143242a790743426
priyowkk <anggabuduran@gmail.com>
2024-09-03 23:05:42 +0700

    Add files via upload

---
+++ nasted loops/nasted loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasted_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How Many Rows?:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How Many Columns?:");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What Symbol?:");
+            string symbol = Console.ReadLine();
+
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ num guess/num guess/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num_guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r= new Random();
+            bool playagain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            string response;
+
+            while (playagain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = r.Next(min,max + 1);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("guess a number between " + min + "-" + max + ":");
+                    guess = Convert .ToInt32(Console.ReadLine());
+                    Console.WriteLine("guess :" + guess );
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + "is too high!");
+                    }
+                    else if (guess < number) 
+                    {
+                        Console.WriteLine(guess + " is to low!");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("number: " + number);
+                Console.WriteLine("YOU WIN!!");
+                Console.WriteLine("guesses:" + guesses );
+
+                Console.WriteLine("Would u like to play again? (yes/no) :");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response =="yes")
+                {
+                    playagain = true;
+                }
+                else
+                {
+                    playagain = false;
+                }
+
+            }
+            Console.WriteLine("Thanks For Playing!!!");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
