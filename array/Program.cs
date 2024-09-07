285219bf69ea7a4bee851682406797f492087375
priyowkk <anggabuduran@gmail.com>
2024-09-07 22:21:42 +0700

    Add files via upload

---
+++ array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] cars = new string[3];
+
+        cars[0] = "kontol";
+        cars[1] = "asu";
+        cars[2] = "memek";
+
+        for (int i = 0;i < cars.Length;i++)
+        {
+            Console.WriteLine(cars[i]);
+
+        }
+        Console.ReadKey();
+
+    }
+}
