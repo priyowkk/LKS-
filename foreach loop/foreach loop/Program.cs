2a546e1ea3890a075c0f7a5fdec42ccc9443f168
priyowkk <anggabuduran@gmail.com>
2024-09-08 08:22:14 +0700

    Add files via upload

---
+++ foreach loop/foreach loop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] cars = { "bmw", "msutang", "corvette" };
+        
+
+        foreach (string car in cars)
+        {
+            Console.WriteLine.(car);
+        }
+        Console.ReadKey();
+    } 
+}
