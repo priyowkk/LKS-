6a832f6cb6143bfc422e342cfc7df24398814a9f
priyowkk <anggabuduran@gmail.com>
2024-09-08 15:08:49 +0700

    Add files via upload

---
+++ overloading method/overloading method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double total;
+        total Multiply(2,3,4);
+
+        Console.WriteLine(total);
+        Console.ReadKey();
+    }
+
+    static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    static double Multiply(double a,double b, double c)
+    {
+        return a * b * c;
+    }
+
+}
