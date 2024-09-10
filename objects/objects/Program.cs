497632e895aa1cac6b12dca1d805c7d13b04a360
priyowkk <anggabuduran@gmail.com>
2024-09-11 06:37:20 +0700

    Add files via upload

---
+++ objects/objects/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        human human1 = new human();
+
+        human1.name = "Angga";
+        human1.age = 15;
+        human1.eat();
+        human1.sleep();
+        Console.ReadKey();
+    }
+
+}
+
+class human
+{
+    public String name;
+    public int age;
+
+    public void eat ()
+    {
+        Console.WriteLine(name + " is eating");
+    }
+    public void sleep ()
+    {
+        Console.WriteLine(name + " is sleeping");
+    }
+}
