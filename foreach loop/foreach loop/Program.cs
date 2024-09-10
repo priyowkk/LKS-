3d6d71b3ccb461ec29798b1e4567663c14cac9af
priyowkk <anggabuduran@gmail.com>
2024-09-10 10:47:47 +0700

    Add files via upload

---
+++ foreach loop/foreach loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("who??"); 
+        
+        string name = Console.ReadLine();
+        
+
+
+        string capital = name.ToUpper();
+       // Console.WriteLine(capital);
+
+
+        singhappybirthday(capital);
+        Console.ReadKey();
+    } 
+    static void singhappybirthday(string capital)
+    {
+        Console.WriteLine("HAPPY BIRTH DAY TO YOUUUUU!!!!!");
+        Console.WriteLine("HAPPY BIRTH DAY TO YOUUUUU!!!!!");
+        Console.WriteLine("HAPPY BIRTH DAY TO YOUUUUU!!!!!");
+        Console.WriteLine("HAPPY BIRTH DAY DEAR YOUUUUU!!!!!" + capital);
+        Console.WriteLine("HAPPY BIRTH DAY TO YOUUUUU!!!!!");
+    }
+}
