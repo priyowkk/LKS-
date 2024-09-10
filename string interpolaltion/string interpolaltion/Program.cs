1713261c097d008e82c4b15f7fbf628a53b531b3
priyowkk <anggabuduran@gmail.com>
2024-09-10 20:08:41 +0700

    Add files via upload

---
+++ string interpolaltion/string interpolaltion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nama Depan :");
+        string FN = Console.ReadLine();
+
+        Console.Write("Nama Belakang :");
+        string LN = Console.ReadLine();
+
+        Console.Write("usia :");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        
+
+        //Console.WriteLine("hello" + FN + " " + LN);
+        //Console.WriteLine("you are " + age  + "years old");
+
+        Console.WriteLine($"hello {FN} {LN}.");
+        Console.WriteLine($"you are {age} years old");
+    }
+}
