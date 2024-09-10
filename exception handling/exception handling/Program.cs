50477a2d3ab0137bcbb4cb4cf8481f0246229cc0
priyowkk <anggabuduran@gmail.com>
2024-09-10 10:46:40 +0700

    Add files via upload

---
+++ exception handling/exception handling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x;
+        int y;
+        double hasil;
+        try
+        {
+            Console.Write("enter number 1: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter number 2: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            hasil = x / y;
+
+            Console.WriteLine("hasil :" + hasil);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("enter only numbers plase!");
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("u can't divide by zero IDIOT!!");
+        }
+
+        catch (Exception e) 
+        {
+            Console.WriteLine("Something Gone Wrong!!");
+        }
+        Console.ReadKey();
+    }
+}
