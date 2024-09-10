8d697a66609eef4dc379bc810302c866b08c88b7
priyowkk <anggabuduran@gmail.com>
2024-09-10 10:51:58 +0700

    Add files via upload

---
+++ params keyword/params keyword/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double total = CheckOut(3.99, 5.75, 15);
+        Console.WriteLine(total);
+        Console.ReadKey();
+
+    }
+    static double CheckOut(params double[] prices)
+    {
+        double total = 0;
+        foreach(double price in prices)
+        {
+            total += price;
+        }
+        return total;
+    }
+
+}
