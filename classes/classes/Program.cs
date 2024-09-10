2b3aa0c365fc9795f372815dd5639fc6a0a5f33f
priyowkk <anggabuduran@gmail.com>
2024-09-10 21:12:11 +0700

    Add files via upload

---
+++ classes/classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Math.Round(3.14);
+        //Math.Abs(-3.14);
+        //Math.Pow(2,3);
+
+        Console.ReadKey();
+    }   
+    class Messege
+    {
+        void Hello ()
+        {
+            Console.WriteLine("Hello welcome to the program");
+
+        }
+        void Waiting ()
+        {
+            Console.WriteLine("i am waiting for something");
+        }
+        void bye()
+        {
+            Console.WriteLine("Good Bye!!");
+        }
+    }
+}
