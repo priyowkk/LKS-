fbbd53ec7bcd5d197ea66f1c3d2c3cc18a3f0fcf
priyowkk <anggabuduran@gmail.com>
2024-09-11 13:17:01 +0700

    Add files via upload

---
+++ static/static/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Car car1 = new Car("Mustang");
+        Car car2 = new Car("Corvette");
+        Car car3 = new Car("Avanza");
+
+        Console.WriteLine(Car.numberOfCars);
+        Car.StartRace();
+
+
+
+        Console.ReadKey();
+    }
+}
+class Car
+{
+    String model;
+    public static int numberOfCars;
+
+    public Car(String model)
+    {
+        this.model = model;
+        numberOfCars ++;
+    }
+    public static void StartRace()
+    {
+        Console.WriteLine("the race has begun!!");
+    }
+}
