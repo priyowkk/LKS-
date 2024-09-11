6ce49004b1daee15c0ba6f1cf63bbea5cf8713b2
priyowkk <anggabuduran@gmail.com>
2024-09-11 20:49:06 +0700

    Add files via upload

---
+++ inheritance/inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Inheritace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Cycle cycle = new Cycle();
+            Boat boat = new Boat();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
+
+            Console.WriteLine(cycle.speed);
+            Console.WriteLine(cycle.wheels);
+            cycle.go();
+
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            boat.go();
+
+
+            Console.ReadKey();
+        }
+    }
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This Vehicle Is Moving!!");
+        }
+    }
+    class Car:Vehicle
+    {
+        public int wheels = 4;
+    }
+    class Cycle : Vehicle
+    {
+        public int wheels = 2;
+    }
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+    }
+}
