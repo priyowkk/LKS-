5cc8551a50f97144c644ec8689e44ffdd33329bd
priyowkk <anggabuduran@gmail.com>
2024-09-12 21:54:39 +0700

    Add files via upload

---
+++ array of objects/array of objects/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] garage = new Car[3] ;
+
+            Car car1 = new Car("avanza");
+            Car car2 = new Car("Lambo");
+            Car car3 = new Car("yaris");
+
+            garage[0] = car1;
+            garage [1] = car2;
+            garage[2] = car3;
+
+            foreach (Car car in garage)
+            {
+                Console.WriteLine(car.model);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        public String model;
+
+        public Car(String model)
+        {
+            this.model = model;
+        }
+
+    }
+}
