b7afa8380ca7d9482b90824d6dca9abbe7cf5c8f
priyowkk <anggabuduran@gmail.com>
2024-09-13 10:46:13 +0700

    Add files via upload

---
+++ polymorph/polymorph/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+            Bike bike = new Bike();
+            Boat boat = new Boat();
+
+            Vehicle[] vehicles = { car, bike, boat };
+
+            foreach(Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+class Vehicle
+{
+    public virtual void Go()
+    {
+
+    }
+}
+ class Car : Vehicle
+ { 
+    public override void Go()
+    {
+        Console.WriteLine("the car is moving");
+    }
+ }
+class Bike : Vehicle   
+{
+    public override  void Go()
+    {
+        Console.WriteLine("the bike is moving");
+    }
+}
+class Boat : Vehicle
+{
+    public override void Go()
+    {
+        Console.WriteLine("the boat is moving");
+    }
+}
