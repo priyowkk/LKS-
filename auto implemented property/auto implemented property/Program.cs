9390469c83cb1a7b1ee2712a8aca9ce0bde136a2
priyowkk <anggabuduran@gmail.com>
2024-09-13 19:39:00 +0700

    Add files via upload

---
+++ auto implemented property/auto implemented property/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car ("porsche");
+            Console.WriteLine(car.Model );
+            Console.ReadKey();
+        }
+    }
+}
+class Car
+{
+
+    public String Model { get; set; }
+
+    public Car(String model)
+    {
+        this.Model = model;
+    }
+}
+
+
