effb4bafeeb46f982d03a437332d0e096a2e4076
priyowkk <anggabuduran@gmail.com>
2024-09-13 19:24:07 +0700

    Add files via upload

---
+++ getters and setters/getters and setters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(400);
+
+            car.Speed = 1000000; //limited
+            Console.WriteLine(car.Speed);
+            Console.ReadKey();
+        }
+    }
+}
+class Car
+{
+    private int speed;
+
+    public Car (int speed)
+    {
+        Speed = speed;
+    }
+    public int Speed
+    {
+        get { return speed; }
+        set
+        {
+            if (value >500)
+            {
+                speed = 500;
+            }
+            else
+            {
+                speed = value;
+            }
+            speed = value;
+        }
+    }
+}
