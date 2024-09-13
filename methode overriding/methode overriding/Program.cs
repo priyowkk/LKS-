dfece65b618ad3e66f42077bb278358c23eef5f4
priyowkk <anggabuduran@gmail.com>
2024-09-13 10:21:45 +0700

    Add files via upload

---
+++ methode overriding/methode overriding/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Speak();
+            cat.Speak();
+            Console.ReadKey();
+        }
+    }
+}
+ class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("the animal goes brrrr");
+    }
+}        
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("the doh goes wooff");
+    }
+}
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("the cat goes miawwww");
+    }
+}
