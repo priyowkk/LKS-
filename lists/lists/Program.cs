d37da61e77dd2fbbd3cb75a6d273e705c7bc4f27
priyowkk <anggabuduran@gmail.com>
2024-09-13 19:07:24 +0700

    Add files via upload

---
+++ lists/lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LKS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> food  = new List<string>();
+
+            food.Add("pizza");
+            food.Add("hamburger");
+            food.Add("hotdog");
+            food.Add("fries");
+
+
+            //food.Remove("fries");
+
+            foreach (string item  in food)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
